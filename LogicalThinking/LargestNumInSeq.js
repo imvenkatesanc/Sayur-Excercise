18a9223494144ef0ddfcb676c0c382fb10100962
imvenkatesanc <venkatesanit2020@gmail.com>
2023-11-04 12:15:18 +0530

    collatz done!

---
+++ LogicalThinking/LargestNumInSeq.js
@@ -0,0 +1,3 @@
+// #Problem #3
+// Get a input. Create a sequence of numbers from that input using the above alg.
+// Find the largest number in the sequence. 
---
+++ LogicalThinking/LessStepToReach1.js
@@ -0,0 +1,4 @@
+// Problem #2
+// Same as above.
+// Input two numbers.
+// Print which number has less steps to reach 1.
