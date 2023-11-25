916a6b34cb1fe6ef5231c07b24661d27dac9b09f
imvenkatesanc <venkatesanit2020@gmail.com>
2023-11-25 11:51:35 +0530

    needs were setup with day1 exercise!

--- DailyExercises/Day2/Problem1.js
+++ DailyExercises/Day2/Problem1.js
@@ -7,11 +7,3 @@
 // 543212345
 // Get user input for how far to go (up to 0)
 
-function toGetRow(rows){
-    for(let i =1; i<rows; i++){
-        for(let j=0; j<rows; j++){
-            let pattern = '';
-        }
-        
-    }
-}
