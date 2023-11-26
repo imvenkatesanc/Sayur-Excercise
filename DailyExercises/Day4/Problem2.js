8a21972669b487147216fa646aa0a1de118bcbd5
imvenkatesanc <venkatesanit2020@gmail.com>
2023-11-26 14:26:22 +0530

    Day4 is done!

--- DailyExercises/Day4/Problem2.js
+++ DailyExercises/Day4/Problem2.js
@@ -9,3 +9,70 @@
 // Player who gets 5 points first wins the game.
 // Print the board each time after the user rolls and also print the current score.
 // Use functions
+
+let boardSize = 6;
+let gameBoard = playerBoard(boardSize);
+let playerAScore = 0;
+let playerBScore = 0;
+
+function playerBoard(boardSize){
+    let cell=[];
+    for(let i=0; i<boardSize; i++){
+        cell[i] = new Array(boardSize).fill('*');
+    }
+    return cell;
+}
+
+function printBoard(){
+    console.log("Game Board: ");
+    for(let row of gameBoard){
+        console.log(row.join(' '));
+    }
+}
+
+function rollDice(){
+    let dice = Math.floor(Math.random()*6)+1;
+    return dice;
+}
+
+function gamePlay(){
+    while(playerAScore<5 && playerBScore<5){
+        console.log('\nPayer A\'s Turn');
+        playTurn('A');
+        printBoard();
+        console.log('A score-',playerAScore, ' B score-',playerBScore);
+        
+        console.log('\nPayer B\'s Turn');
+        playTurn('B');
+        printBoard();
+        console.log('A score-',playerAScore, ' B score-',playerBScore);
+    }
+
+    if (playerAScore === 5) {
+        console.log('Player A wins!');
+    } else {
+        console.log('Player B wins!');
+    }
+}
+
+function playTurn(player) {
+    const row = rollDice() - 1;
+    const col = rollDice() - 1;
+
+    console.log(`Rolled: (${row + 1}, ${col + 1})`);
+
+    if (gameBoard[row][col] === '*') {
+        gameBoard[row][col] = player;
+    } else {
+        console.log(`Player ${player} gets a point!`);
+        if (player === 'A') {
+            playerAScore++;
+        } else {
+            playerBScore++;
+        }
+        gameBoard[row][col] = player;
+    }
+}
+
+// Start the game
+gamePlay();
